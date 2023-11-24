f66ce0cdc9801abcd96a4793330587c79bc25b3e
Okyere Amponsah Kwatia <77788839+orionty@users.noreply.github.com>
2023-11-24 15:50:08 +0000

    second commit

--- app.js
+++ app.js
@@ -1,65 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const cors = require('cors');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import Connection from './database/db.js';
+import Routes from './routes/route.js';
 
 const app = express();
-const port = 3001;
 
-app.use(bodyParser.json());
+
 app.use(cors());
 
-const TASKS_FILE_PATH = 'tasks.txt';
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use('/', Routes);
 
-app.post('/saveTasks', (req, res) => {
-  const tasks = req.body;
-  const textTasks = tasks.map((task) => `${task.date} ${task.time}  ${task.text}`).join('\n');
+const PORT = 8000;
 
-  fs.writeFile(TASKS_FILE_PATH, textTasks, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to save tasks' });
-    } else {
-      res.json({ success: true });
-    }
-  });
-});
+Connection();
 
-app.get('/getTasks', (req, res) => {
-  fs.readFile(TASKS_FILE_PATH, 'utf8', (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        res.json([]); // File doesn't exist, return an empty array
-      } else {
-        console.error(err);
-        res.status(500).json({ error: 'Failed to retrieve tasks' });
-      }
-    } else {
-      const tasks = data
-        .split('\n')
-        .filter(Boolean)
-        .map((line) => {
-          const [date, time, ...textArray] = line.split(' ');
-          const completedIndex = textArray.indexOf('-');
-          const completed = completedIndex !== -1 && textArray[completedIndex + 1] === 'Completed';
-          return {
-            date,
-            time,
-            text: completed ? textArray.slice(0, completedIndex).join(' ') : textArray.join(' '),
-            completed,
-          };
-        });
-
-      res.json(tasks);
-    }
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-
+app.listen(PORT, () => console.log(`Your server is running successfully on PORT ${PORT}`));
